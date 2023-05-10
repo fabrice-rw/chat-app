275d35cbca7466239dc01bfc6b3c33bf4dca637a
fabrice-rw <fabricenzayisenga@gmail.com>
2023-05-11 01:08:33 +0200

    initial commit

---
+++ typeDefs.js
@@ -0,0 +1,29 @@
+import { gql } from 'apollo-server'
+
+
+const typeDefs = gql`
+    type Query{
+        users:[User]
+        user(id:ID!):User
+    }
+
+    input UserInput{
+        firstName:String!
+        lastName:String!
+        email:String!
+        password:String!
+    }
+
+    type Mutation{
+    
+    }
+
+    type User{
+        id:ID!
+        firstName:String!
+        lastName:String!
+        email:String!
+    }
+`
+
+export default typeDefs;
